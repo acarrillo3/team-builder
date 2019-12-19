97c71819c91e1c780f48157106dc3999ed5f8281
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-18 22:40:07 -0500

    created the form where i type my name, age, and role and it appears in the hooks of mydevtools

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import Form from './Components/Form'
 
 function App() {
+  const [members, setTeamMember] = useState([
+    {
+      name:"Test",
+      age: "Test",
+      role:"test"
+    }]);
+
+    const [toEdit, setToEdit] = useState();
+//the above is used in line 29 to edit the members information
+    const addNewMember = (member) => {
+      const newMember = {
+        name: member.name,
+        age: member.age,
+        role: member.role
+      }
+      setTeamMember([...members, newMember])
+    }
+    //the above are two array functions one where we create our first member and the second one to is to add a new member but keeping the older member(s) in count as you see in line 22
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Our Team:</h1>
+      <Form addNewMember={addNewMember} setTeamMember={setTeamMember} toEdit={toEdit} />
     </div>
   );
 }
